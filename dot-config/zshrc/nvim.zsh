cc72504143d88031e63dbd3b1d6098081a24d676
Benjamin Tanguay <benjamin.tanguay@gmail.com>
2025-01-30 23:26:14 -0500

    Better algo to switch nvim configs

--- dot-config/zshrc/nvim.zsh
+++ dot-config/zshrc/nvim.zsh
@@ -1,13 +1,15 @@
 function nvims() {
         config=$(find -L "${XDG_CONFIG_HOME:-$HOME/.config}" -mindepth 2 -maxdepth 2 -name init.lua -o -name init.vim | \
-        awk -F/ '{print $(NF-1)}' | \
-        fzf --prompt 'Neovim config' --layout=reverse --border --exit-0)
+          awk -F/ '{print $(NF-1)}' | \
+          fzf --prompt 'Neovim config' --layout=reverse --border --exit-0)
         if [[ -z $config ]]; then
           echo "Nothing selected"
           return 0
         elif [[ $config == "default" ]]; then
           config=""
+        else
+          export NVIM_APPNAME="${config}"
         fi
-        NVIM_APPNAME=$config nvim $@
+        nvim $@
 }
 alias ns=nvims
